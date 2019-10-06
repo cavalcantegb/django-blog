c00f741c79bd38c6ac18216b56c325bc7b1efce5
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-10-06 18:24:53 +0000

    Customizing post admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
