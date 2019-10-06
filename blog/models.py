41c4e70af187c95103f31adc83ce48b3ac304631
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-10-06 18:46:45 +0000

    Customizing model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -26,3 +30,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
