305a95460bd6360a7b18964e494ca16c58865377
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-10-06 19:07:25 +0000

    Building views

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,6 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
