75c9163c87ee6c937fcd50356d3049910f3da9a4
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-10-06 19:08:00 +0000

    Building views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
